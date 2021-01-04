0354d0e01578cb54ececd130efbeedd4e1b6cf91
stev <623565916@qq.com>
2021-01-04 13:49:33 +0800

    43

--- src/main/java/com/example/demospringboot/DemospringbootApplication.java
+++ src/main/java/com/example/demospringboot/DemospringbootApplication.java
@@ -3,8 +3,6 @@ package com.example.demospringboot;
 import com.example.demospringboot.mapper.PersonMapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,21 +12,21 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 //@ComponentScan("com.example.demospringboot")
 //public class DemospringbootApplication implements ApplicationRunner {
-public class DemospringbootApplication implements ApplicationRunner {
+public class DemospringbootApplication {
     @Autowired
     private PersonMapper personMapper;
     public static void main(String[] args) {
-        SpringApplication.run(DemospringbootApplication.class, args);
+//        SpringApplication.run(DemospringbootApplication.class, args);
         System.out.println("12345");
     }
 
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        System.out.println("12345");
-//        List<Person> personList = personMapper.selectList(null);
+//    @Override
+//    public void run(ApplicationArguments args) throws Exception {
 //        System.out.println("12345");
-
-    }
+////        List<Person> personList = personMapper.selectList(null);
+////        System.out.println("12345");
+//
+//    }
 
 
 
